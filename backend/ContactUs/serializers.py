14c26c229c7ed0cdc7cd551a1b7420266be5760a
Mohamed-Elfar <st2020048@kfs-hiet.edu.eg>
2025-07-27 21:28:34 +0300

    add book appiontment pages removed back

--- backend/ContactUs/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ContactUs
-
-class ContactUsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactUs
-        fields = ['name', 'national_id', 'message']
--- backend/Prescription/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AddPrescriptionView
-
-urlpatterns = [
-    path('patients/<str:patient_national_id>/prescriptions/', AddPrescriptionView.as_view(), name='add_prescription'),
-]
