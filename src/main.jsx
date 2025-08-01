c8ea5ab0f9bcbaa109dc0de161622b99c5ef3d20
Mohamed Magoor <mohamedmagoor6@gmail.com>
2025-08-01 19:05:55 +0300

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/reset.css";
 
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'antd/dist/reset.css'
+import "./index.css";
 
-import './index.css'
-
-
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
-)
+createRoot(document.getElementById("root")).render(<App />);
